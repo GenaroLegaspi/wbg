55a23057261dde10e8881a9535fc4303326d7ed9
rgberou <brotark@gmail.com>
2015-02-20 11:21:35 -0800

    tan aw a

---
+++ login.php
@@ -0,0 +1,91 @@
+<html>
+	<head>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/newstyle.css">
+
+
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	</head>
+	<title> World Be Green- </title>
+	<body>
+		<nav class="navbar navbar-default">
+  		<div class="container-fluid">
+  			<div class="col-md-5">
+    			<div class="navbar-header">
+      				<h1>World Be Green</h1>	
+    			</div>
+    		</div>
+    		<div class="col-md-7">
+    			<br>
+    			<form class="form-inline">
+  						<div class="form-group">
+    						<label for="exampleInputName2">Username</label>
+    						<input type="text" class="form-control" id="exampleInputName2">
+  						</div>
+  						<div class="form-group">
+    						<label for="exampleInputName2">Password</label>
+    						<input type="text" class="form-control" id="exampleInputName2" >
+  						</div>
+  						<button type="submit" class="btn btn-default">Send invitation</button>
+  				</form>
+    		</div>
+  		
+		</nav>
+		<ul class="nav nav-tabs">
+  			<li role="presentation" class="active"><a href="#">Home</a></li>
+  			<li role="presentation"><a href="#">Benefeciaries</a></li>
+  			<li role="presentation"><a href="#">Events</a></li>
+		</ul>
+    <div class="container">
+    <div class="rows">
+      <div class="col-md-12">
+            <div class="homepic">
+              <div class="col-md-7">
+
+              </div>
+
+            </div>
+          
+               
+               <div class="col-md-4">
+                <div class="signup">
+                  <br>
+                  <form>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Name</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Email</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Confirm Password</label>
+                      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password">
+                    </div>
+                      <center>
+                       <button type="submit" class="btn btn-default">Submit</button>
+                       </center>
+                    </form>.
+                    </div>
+                </div>
+        </div>
+      </div>
+    </div>
+        <footer>
+            <div class="foot">
+                <h5>Brought you by Microtron</h5>
+            </div>
+        </footer>           
+
+
+      
+
+
+	</body>
+</html>
