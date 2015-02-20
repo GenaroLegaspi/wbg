24615acbb613efe7d57027906021a8253da31d81
GenaroLegaspi <narz_ipsagel@yahoo.com>
2015-02-20 11:26:10 +0800

    new changes

--- query.php
+++ query.php
@@ -13,8 +13,8 @@
 			$address = "Region 7";
 			$status = 1;
 			
-			$com = "INSERT INTO accts(acctid,accttype,name,email,password,password2,company,address,status)VALUES('$acctid','$accttype','$name','$email','$password','$password2','$company','$address','$status')"
+			$com = "INSERT INTO accts(acctid,accttype,acctname,email,password,password2,company,address,status)VALUES('$acctid','$accttype','$name','$email','$password','$password2','$company','$address','$status')";
 			mysql_query($com);
-			echo "<script> alert ('You are now registered!!!')</script>"
+			echo "<script>alert('You are now registered!!!')</script>"
 
 ?>
